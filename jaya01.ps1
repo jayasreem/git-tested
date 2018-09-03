1261ca405f8e66b778693a790373c003c64bb421
jayasree <you@example.com>
2018-09-03 23:39:21 +0530

    st

---
+++ jaya01.ps1
@@ -0,0 +1,30 @@
+﻿Param(
+  [parameter(mandatory=$true)][string]$folderPath,
+  [parameter(mandatory=$true)][string]$findString,
+  [parameter(mandatory=$true)][string]$replaceString
+)
+
+$files=Get-ChildItem $folderPath| Select-Object Name
+    
+    Foreach($file in $files)
+    {
+           $fileName= $file.Name
+           $fileContent = Get-Content $folderPath'\'$fileName
+           $fileContent -replace $findString , $replaceString | Set-Content $folderPath'\'$fileName
+           }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
